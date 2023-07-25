04dc874d8296d3a6a16b27a9f5540bf37c3e9251
Jeffry Rojas <jeffrysnneyder.rr@gmail.com>
2023-07-24 22:26:26 -0500

    flip

--- main.js
+++ main.js
@@ -68,11 +68,51 @@ function createPokemon( pokemon){
 
     const cardBack = document.createElement('div');
     cardBack.classList.add('pokemon-block-back');
-    cardBack.textContent = "Carta Atras";
+    cardBack.textContent = "Stats";
+
+    cardBack.appendChild(progressBars(pokemon.stats));
 
     cardContainer.appendChild(card);
     cardContainer.appendChild(cardBack);
     pokemonContainer.appendChild(flipCard);
+}
+function pokemonType(type){
+
+}
+function progressBars (stats){
+    const statsContainer = document.createElement('div');
+    statsContainer.classList.add('stats-container');
+
+    for(let i=0;i< 6; i++){
+        const stat = stats[i];
+
+        const statPercent = stat.base_stat / 2 + "%";
+        const statContainer = document.createElement('div');
+        statContainer.classList.add('stat-container');
+
+        const statName = document.createElement('div');
+        statName.textContent = stat.stat.name;
+        
+        const progress = document.createElement('div');
+        progress.classList.add('progress');
+
+        const progressBar = document.createElement('div');
+        progressBar.classList.add('progress-bar');
+        progressBar.setAttribute('aria-valuenow', stat.base_stat);
+        progressBar.setAttribute('aria-valuemin', 0);
+        progressBar.setAttribute('aria-valuemax', 200);
+        progressBar.style.width = statPercent;
+
+        progressBar.textContent = stat.base_stat;
+
+        progress.appendChild(progressBar);
+        statContainer.appendChild(statName);
+        statContainer.appendChild(progress);
+        statsContainer.appendChild(statContainer);
+        
+    }
+
+    return statsContainer;
 }
 
 function removeChildNodes(parent){
